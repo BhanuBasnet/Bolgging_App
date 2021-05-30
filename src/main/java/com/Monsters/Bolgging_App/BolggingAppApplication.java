477129d5d3cec4f0bb0b204bf5bbf2911bfa7bd1
Bhanu Basnet <bhanubasnet950@gmail.com>
2021-05-30 10:44:20 +0545

    project setup

---
+++ src/main/java/com/Monsters/Bolgging_App/BolggingAppApplication.java
@@ -0,0 +1,13 @@
+package com.Monsters.Bolgging_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolggingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolggingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Monsters/Bolgging_App/BolggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Monsters.Bolgging_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolggingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
